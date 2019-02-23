7ca70aa0e2d8efd136ec707af039f1657a83624b
caiyangmin <18819457397@163.com>
2019-02-23 16:31:45 +0800

    new exercise

---
+++ Algorithms/771 Jewels and Stones/Jewels and Stones.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+}
+
+func numJewelsInStones(J string, S string) int {
+    m := make(map[rune]bool)
+    for _, s := range J {
+        m[s] = true
+    }
+    count := 0
+    for _, s := range S {
+        if m[s] == true {
+            count++
+        }
+    }
+    return count
+}
