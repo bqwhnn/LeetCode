9b13e9f9851ec20a0b596dad8c91e5a36d81b2ec
caiyangmin <18819457397@163.com>
2019-03-07 19:49:32 +0800

    new exam

---
+++ Algorithms/169 Majority Element/Majority Element.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	
+}
+
+func majorityElement(nums []int) int {
+    ret := nums[0]
+    cnt := 0
+    for _, num := range nums {
+        if cnt == 0 {
+            ret = num
+        }
+        if num == ret {
+            cnt++
+        } else {
+            cnt--
+        }
+    }
+    return ret
+}
