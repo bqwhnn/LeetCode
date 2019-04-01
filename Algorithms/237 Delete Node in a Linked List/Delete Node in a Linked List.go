e8c0d11898f0eecdc66f125930c3d8b16cd326ac
caiyangmin <18819457397@163.com>
2019-04-01 21:16:37 +0800

    update

---
+++ Algorithms/237 Delete Node in a Linked List/Delete Node in a Linked List.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+// 把 node下一节点复制到node，把下一节点跳过！
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func deleteNode(node *ListNode) {
+    node.Val = node.Next.Val
+    node.Next = node.Next.Next
+}
