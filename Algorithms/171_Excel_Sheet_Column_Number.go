6bf492a1587a20fa054466e8ac5f59293d7e0c3d
caiyangmin <18819457397@163.com>
2019-04-02 19:12:48 +0800

    change directory structure

---
+++ Algorithms/171_Excel_Sheet_Column_Number.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "ACG"
+	fmt.Println(titleToNumber(s))
+}
+
+// 26 进制
+func titleToNumber(s string)int {
+    n := 0
+    for _, char := range s {
+        n = n * 26 + int(char) - 'A' + 1
+        fmt.Println(char, n)
+    }
+    return n
+}
