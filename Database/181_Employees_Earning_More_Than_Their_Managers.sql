760696b796e9c29c57c7ebaadbbd89090fab11c9
caiyangmin <18819457397@163.com>
2019-04-14 17:53:39 +0800

    new exce

---
+++ Database/181_Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,33 @@
+-- Employee 表包含所有员工，他们的经理也属于员工。每个员工都有一个 Id，此外还有一列对应员工的经理的 Id。
+
+-- +----+-------+--------+-----------+
+-- | Id | Name  | Salary | ManagerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | NULL      |
+-- | 4  | Max   | 90000  | NULL      |
+-- +----+-------+--------+-----------+
+-- 给定 Employee 表，编写一个 SQL 查询，该查询可以获取收入超过他们经理的员工的姓名。在上面的表格中，Joe 是唯一一个收入超过他的经理的员工。
+
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+
+-- sql 架构
+Create table Employee (Id int, Name varchar(255), Salary int, ManagerId int)
+Truncate table Employee
+insert into Employee (Id, Name, Salary, ManagerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (Id, Name, Salary, ManagerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (Id, Name, Salary, ManagerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (Id, Name, Salary, ManagerId) values ('4', 'Max', '90000', 'None')
+
+-- as 重命名列名或则表名
+-- 将 table_a 命名为 table_b
+-- SELECT * FROM table_a as table_b
+-- answer
+SELECT a.Name AS Employee
+FROM Employee a, Employee b
+WHERE a.ManagerId = b.Id AND a.Salary > b.Salary;
